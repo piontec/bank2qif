cd23df5f9c25b6292cb9c2ef6cf3b20a29157e46
piontec <piontec@gmail.com>
2012-10-20 18:31:10 +0200

    * execute and capture output from pdf2xml

--- Sync2Qif/src/PdfToXmlReader.cs
+++ Sync2Qif/src/PdfToXmlReader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Xml.Linq;
+using System.IO;
 
 namespace Sync2Qif
 {
@@ -8,7 +9,20 @@ namespace Sync2Qif
 	{
 		public static XDocument Read (string file)
 		{
-			throw new NotImplementedException ();
+			if (!File.Exists (file))
+				throw new ApplicationException (string.Format("File does not exist: {0}", file));
+
+			Process xml2txt = new Process ();
+			xml2txt.StartInfo.FileName = "pdf2xml";
+			xml2txt.StartInfo.Arguments = string.Format ("-t xml {0}", file);
+			xml2txt.StartInfo.UseShellExecute = false;
+			xml2txt.StartInfo.RedirectStandardOutput = true;
+			xml2txt.Start();
+			
+			string output = xml2txt.StandardOutput.ReadToEnd();
+			xml2txt.WaitForExit();
+
+			return XDocument.Parse (output);
 		}
 	}
 }
