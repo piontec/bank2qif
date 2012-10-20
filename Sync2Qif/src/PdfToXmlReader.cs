567769900f430dd5855d832da15ce93108f8e4ec
piontec <piontec@gmail.com>
2012-10-20 21:34:08 +0200

    * playground stuff

--- Sync2Qif/src/PdfToXmlReader.cs
+++ Sync2Qif/src/PdfToXmlReader.cs
@@ -13,14 +13,25 @@ namespace Sync2Qif
 				throw new ApplicationException (string.Format("File does not exist: {0}", file));
 
 			Process xml2txt = new Process ();
-			xml2txt.StartInfo.FileName = "pdf2xml";
+			xml2txt.StartInfo.FileName = "pdf2txt";
 			xml2txt.StartInfo.Arguments = string.Format ("-t xml {0}", file);
 			xml2txt.StartInfo.UseShellExecute = false;
 			xml2txt.StartInfo.RedirectStandardOutput = true;
-			xml2txt.Start();
-			
-			string output = xml2txt.StandardOutput.ReadToEnd();
-			xml2txt.WaitForExit();
+			string output = "";
+
+			try {
+				xml2txt.Start();				
+				output = xml2txt.StandardOutput.ReadToEnd();
+				xml2txt.WaitForExit();
+			}
+			catch (System.ComponentModel.Win32Exception) {
+				Console.WriteLine ("Error invoking xml2txt parser. Is the program installed?");
+			}
+			catch (Exception e) {
+				Console.WriteLine ("Unexpected error occured while invoking external program xml2txt.");
+				Console.WriteLine (e.Message);
+				throw;
+			}
 
 			return XDocument.Parse (output);
 		}
--- Sync2QifPlayground/src/Main.cs
+++ Sync2QifPlayground/src/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using Sync2Qif;
+using System.Xml.Linq;
 
 namespace Sync2QifPlayground
 {
@@ -6,7 +8,9 @@ namespace Sync2QifPlayground
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Loading file");
+			var xdoc = PdfToXmlReader.Read (@"../../../Sync2QifTests/data/wyciag1.pdf");
+			Console.WriteLine (xdoc);
 		}
 	}
 }
