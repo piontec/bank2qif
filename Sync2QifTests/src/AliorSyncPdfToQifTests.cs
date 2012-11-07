b30123db0897b402676681b861accdd9224a5a4f
piontec <piontec@gmail.com>
2012-11-07 23:12:16 +0100

    ADD: first test foro the parser of alior sync

---
+++ Sync2QifTests/src/AliorSyncPdfToQifTests.cs
@@ -0,0 +1,34 @@
+using System;
+using NUnit.Framework;
+using Sync2Qif.Converters;
+using Sync2Qif;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+
+namespace Sync2QifTests
+{
+	[TestFixture]
+	public class AliorSyncPdfToQifTests
+	{
+		AliorSyncPdfToQif converter;
+		XDocument xml;
+		IList<QifEntry> res;
+
+		[SetUp]
+		public void SetUp ()
+		{
+			xml = XDocument.Load ("../../data/wyciag1.xml");
+			converter = new AliorSyncPdfToQif ();
+			res = converter.ConvertXmlToQif (xml);
+		}
+
+		[Test]
+		public void Should_return_all_operations ()
+		{
+
+			Assert.AreEqual (9, res.Count);
+		}
+	}
+}
+
