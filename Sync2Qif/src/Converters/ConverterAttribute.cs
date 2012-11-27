ac074f87fb82ae2a8f9a1c3317f32bd12dc68710
piontec <piontec@gmail.com>
2012-11-27 21:47:37 +0100

    added ConverterAttribute

---
+++ Sync2Qif/src/Converters/ConverterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sync2Qif.Converters
+{
+    /// <summary>
+    /// Attribute to decorate Converters from a specific bank format to <see cref="QifEntry"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ConverterAttribute : Attribute
+    {
+        private string bank;
+        private string ext;
+
+        /// <summary>
+        /// A default constructor
+        /// </summary>
+        /// <param name="bank">A name of a bank, from which the decorated class is used to convert data.</param>
+        /// <param name="ext">An expected file extension for data files of this bank</param>
+        public ConverterAttribute(string bank, string ext)
+        {
+            this.bank = bank;
+            this.ext = ext;
+        }
+
+        public string Bank { get { return bank; } }
+
+        public string Extension { get { return ext; } }
+    }
+}
