7074a2f7e0d745c97ad953d19fc3b088fe762ea5
piontec <piontec@gmail.com>
2012-12-01 21:05:11 +0100

    ADD: file writing

--- Sync2Qif/src/QifFile.cs
+++ Sync2Qif/src/QifFile.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Bank2Qif
 {
@@ -7,7 +9,18 @@ namespace Bank2Qif
 	{
         public static void Save(IEnumerable<QifEntry> entries, string originalFileName)
         {
-            throw new NotImplementedException();
+            StringBuilder output = new StringBuilder();
+
+            foreach (var entry in entries)
+            {
+                output.Append(entry.ToQifString());
+                output.Append(System.Environment.NewLine);
+            }
+
+            using (StreamWriter file = new StreamWriter (originalFileName, false, Encoding.UTF8))
+            {
+                file.Write(output.ToString());
+            }
         }
 
         private static string GetQifFileName(string originalFileName)
