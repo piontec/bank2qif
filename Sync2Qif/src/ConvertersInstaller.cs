a6aaa4243d18067c59a3df31508841ab275a443a
piontec <piontec@gmail.com>
2012-12-01 15:55:50 +0100

    CHG: Converters registration using windsor only

--- Sync2Qif/src/ConvertersInstaller.cs
+++ Sync2Qif/src/ConvertersInstaller.cs
@@ -6,7 +6,6 @@ using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using Bank2Qif.Converters;
-using System.Reflection;
 
 namespace Bank2Qif
 {
@@ -14,19 +13,30 @@ namespace Bank2Qif
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            var converters = from t in Assembly.GetExecutingAssembly().GetTypes()
-                             where t.IsClass
-                             let iface = t.GetInterface(typeof(IConverter).Name)
-                             where iface != null
-                             let convAttr = from a in Attribute.GetCustomAttributes(t)
-                                            where a is ConverterAttribute
-                                            select (ConverterAttribute)a
-                             select new Tuple<Type, ConverterAttribute> ( t, convAttr.Single () );
+            //var converters = from t in Assembly.GetExecutingAssembly().GetTypes()
+            //                 where t.IsClass
+            //                 let iface = t.GetInterface(typeof(IConverter).Name)
+            //                 where iface != null
+            //                 let convAttr = from a in Attribute.GetCustomAttributes(t)
+            //                                where a is ConverterAttribute
+            //                                select (ConverterAttribute)a
+            //                 select new Tuple<Type, ConverterAttribute> ( t, convAttr.Single () );
             
+            //container.Register(
+            //    converters.Select (t => Component.For(typeof(IConverter)).ImplementedBy(t.Item1).Named(t.Item2.Bank)).
+            //    Cast<IRegistration> ().ToArray ()                
+            //    );
+
             container.Register(
-                converters.Select (t => Component.For(typeof(IConverter)).ImplementedBy(t.Item1).Named(t.Item2.Bank)).
-                Cast<IRegistration> ().ToArray ()                
-                );
+                Classes.FromThisAssembly()
+                .Where(t => typeof(IConverter).IsAssignableFrom(t) &&
+                    Attribute.IsDefined(t, typeof(ConverterAttribute)))                
+                .LifestyleSingleton()
+                .WithService.Base()
+                .Configure(c => c.Named(
+                    ((ConverterAttribute)Attribute.GetCustomAttribute(
+                    c.Implementation, typeof(ConverterAttribute))).Bank))
+                );            
         }
     }
 }
