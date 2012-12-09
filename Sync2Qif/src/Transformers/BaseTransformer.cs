1ae1ae405a646c4a7ea77e66ec6aa04d5b507d51
piontec <piontec@gmail.com>
2012-12-09 11:52:22 +0100

    ADD: BaseTransformer

---
+++ Sync2Qif/src/Transformers/BaseTransformer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nini.Config;
+
+namespace Bank2Qif.Transformers
+{
+    public abstract class BaseTransformer
+    {
+        private const string INI_ENABLED = "Enabled";
+        private bool m_enabled;
+
+        public void Initialize(IConfig config)
+        {
+            m_enabled = config.GetBoolean(INI_ENABLED, true);
+        }
+    }
+}
