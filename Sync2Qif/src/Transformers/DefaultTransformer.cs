6d5615437a72fbc6173e824a2f66a6ceace6bacf
piontec <piontec@gmail.com>
2012-12-09 11:46:09 +0100

    ADD: DeafultTransformer

---
+++ Sync2Qif/src/Transformers/DefaultTransformer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nini.Config;
+
+namespace Bank2Qif.Transformers
+{
+    [Transformer (0)]
+    public class DefaultTransformer : ITransformer
+    {
+        private const string INI_ACCOUNT = "Account";
+        private const string INI_ACCOUNT_DFLT = "QifImport";
+        private readonly string m_accountName;
+        private readonly bool m_enabled;
+
+
+        public DefaultTransformer(IConfig config)
+        {            
+            m_accountName = config.GetString(INI_ACCOUNT, INI_ACCOUNT_DFLT);
+        }
+
+        public IEnumerable<QifEntry> Transform(IEnumerable<QifEntry> entries)
+        {
+            return entries.AsParallel().Select(e => { e.AccountName = m_accountName; return e; });
+        }
+    }
+}
