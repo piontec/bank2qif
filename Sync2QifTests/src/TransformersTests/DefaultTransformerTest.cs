7d7e01ed59dd4114bcd66a85abd7a4664159dbb5
piontec <piontec@gmail.com>
2012-12-14 22:07:13 +0100

    ADD: defaulttransformer test

---
+++ Sync2QifTests/src/TransformersTests/DefaultTransformerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Bank2Qif;
+using Bank2Qif.Transformers;
+using Nini.Config;
+using Moq;
+
+namespace Sync2QifTests.TransformersTests
+{    
+    [TestFixture]
+    public class DefaultTransformerTest
+    {        
+        private readonly QifEntry entry = new QifEntry 
+        {
+            Description = "Name",            
+        };
+        private IConfig m_config;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mockConfig = new Mock<IConfig>();
+            mockConfig.Setup(c => c.GetString("Account", "QifImport")).Returns("QifImport");
+            m_config = mockConfig.Object;
+        }
+
+
+        [Test]
+        public void TestIsDefaultAssigned()
+        {
+            var transformer = new DefaultTransformer(m_config);
+            var res = transformer.Transform(new List<QifEntry> { entry });
+
+            Assert.AreEqual("QifImport", res.Single().AccountName);
+        }
+    }
+}
