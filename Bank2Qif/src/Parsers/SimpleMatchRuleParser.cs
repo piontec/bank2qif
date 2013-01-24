ea54c45c60a19a73dd3783852bedd6ee42f1aaab
piontec <piontec@gmail.com>
2013-01-24 23:19:06 +0100

    ADD: stub for SimpleMatchTransformer

---
+++ Bank2Qif/src/Parsers/SimpleMatchRuleParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sprache;
+
+namespace Bank2Qif.Parsers
+{
+    public static class SimpleMatchRuleParser
+    {
+        // wildcard
+        // Payee % "text do znalezienia" -> nazwa_konta
+        // exact
+        // Description = "text do znalezienia" -> nazwa_konta
+        // Any % "text
+    }
+}
---
+++ Bank2Qif/src/Transformers/SimpleMatchTransformer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nini.Config;
+using System.IO;
+
+namespace Bank2Qif.Transformers
+{
+    [Transformer(100)]
+    public class SimpleMatchTransformer : BaseTransformer, ITransformer
+    {
+        private static readonly string s_rulesFile;
+        private static readonly string CFG_RULES_FILE_NAME = "RulesFile";
+        private static readonly string CFG_RULES_FILE_NAME_DFLT = "rules.txt";
+
+        public SimpleMatchTransformer(IConfig config)
+        {
+            base.Initialize(config);
+            s_rulesFile = m_config.GetString(CFG_RULES_FILE_NAME, CFG_RULES_FILE_NAME_DFLT);
+        }
+
+
+        public IEnumerable<QifEntry> Transform(IEnumerable<QifEntry> entries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
