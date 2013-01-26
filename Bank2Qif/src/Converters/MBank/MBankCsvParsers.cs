fd6e95e34908d314c1a188b43e6e8ae30f6293d9
piontec <piontec@gmail.com>
2013-01-26 14:36:58 +0100

    CHG: completed mbank parser

--- Bank2Qif/src/Converters/MBank/MBankCsvParsers.cs
+++ Bank2Qif/src/Converters/MBank/MBankCsvParsers.cs
@@ -13,13 +13,15 @@ namespace Bank2Qif.Converters.MBank
     //2011-12-30;2012-01-01;ZAKUP PRZY UŻYCIU KARTY;"STACJA WIELKOPOLSKA/POZNAN";"  ";'';-10,95;1,50;
     public static class MBankCsvParsers
     {
-        private static readonly string START_LINE = "#Data operacji;#Data księgowania;#Opis operacji;#Tytuł;#Nadawca/Odbiorca;#Numer konta;#Kwota;#Saldo po operacji;\n";
+        private static readonly string START_LINE = "#Data operacji;#Data księgowania;#Opis operacji;#Tytuł;#Nadawca/Odbiorca;#Numer konta;#Kwota;#Saldo po operacji;\r\n";
+
         public static readonly Parser<decimal> Amount =
             from minus in Parse.String("-").Text().Or(Parse.Return(string.Empty))
-            from whole in Parse.Number
+            from triples in GenericParsers.OptionalSpaceThenNumber.Token().Many()
             from separator in Parse.Char(',').Once()
-            from pointPart in Parse.Number
-            let strDecimal = string.Format("{0}{1}.{2}", minus, whole, pointPart)
+            from pointPart in Parse.Number.Once()
+            let strDecimal = string.Format("{0}{1}.{2}", minus, triples.Aggregate((s1, s2) => s1 + s2),
+                pointPart.Single())
             select decimal.Parse(strDecimal, CultureInfo.InvariantCulture);
 
         public static readonly Parser<QifEntry> QifEntryParser =
@@ -28,25 +30,29 @@ namespace Bank2Qif.Converters.MBank
             from dateBook in GenericParsers.DateYyyyMmDd
             from sep2 in Parse.Char(';').Once()
             from type in Parse.AnyChar.Until(Parse.Char(';').Once()).Text()
-            from desc in Parse.AnyChar.Until(Parse.Char(';').Once()).Text()
+            from description in Parse.AnyChar.Until(Parse.Char(';').Once()).Text()
             from receiver in Parse.AnyChar.Until(Parse.Char(';').Once()).Text()
             from receiverAcc in Parse.AnyChar.Until(Parse.Char(';').Once()).Text()
             from amount in Amount
             from sep3 in Parse.Char(';').Once()
             from balance in Amount
-            from trailing in Parse.String(";\n")
+            from trailing in Parse.String(";\r\n")            
+            let desc = description.Trim ('"')
             select new QifEntry
             {
                 AccountName = receiverAcc.Trim ('\''),
                 Amount = amount,
                 Date = new BankDates { OperationDate = dateTr, BookingDate = dateBook },
-                Payee = receiver.Trim ('\"'),
-                Description = string.Format ("{0} - {1}", desc, type)
+                Payee = receiver.Trim ('"').Trim(' '),
+                Description = desc.Trim () == string.Empty ?
+                    string.Format("{1}", type) :
+                    string.Format("{0} - {1}", desc, type)
             };
 
         public static readonly Parser<IEnumerable<QifEntry>> QifEntriesParser =
-            from firstLine in Parse.AnyChar.Until(Parse.String (START_LINE))
-            from entries in QifEntryParser.Many().End()
+            from firstLine in Parse.AnyChar.Until(Parse.String (START_LINE))            
+            from entries in QifEntryParser.Many()
+            from trailing in Parse.String("\r\n\r\n;;;;;;")
             select entries;	
     }
 }
--- Bank2Qif/src/Converters/MBank/MBankCsvToQif.cs
+++ Bank2Qif/src/Converters/MBank/MBankCsvToQif.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,24 @@ namespace Bank2Qif.Converters.MBank
         public IEnumerable<QifEntry> ConvertFileToQif(string fileName)
         {
             string lines = File.ReadAllText(fileName, System.Text.Encoding.Default);
-            return MBankCsvParsers.QifEntriesParser.Parse(lines);
+            var entries = MBankCsvParsers.QifEntriesParser.Parse(lines);
+
+            TextInfo myTI = new CultureInfo("pl-PL", false).TextInfo;
+            // normalize many whitespaces into one, do camel casing
+            string pattern = @"Xx(?<num>\d+)xx";
+            string replacement = "XX${num}XX";            
+            foreach (var entry in entries)
+            {
+                entry.Description = System.Text.RegularExpressions.Regex.Replace(
+                    myTI.ToTitleCase(entry.Description.ToLower()), @"\s+", " ");
+                // fix PayU IDs
+                entry.Description = System.Text.RegularExpressions.Regex.Replace(
+                    entry.Description, pattern, replacement);
+                entry.Payee = System.Text.RegularExpressions.Regex.Replace(
+                    myTI.ToTitleCase(entry.Payee.ToLower ()), @"\s+", " ");
+            }
+
+            return entries;
         }
     }
 }
