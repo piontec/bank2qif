e8d37c3bc1b2c323d12273bdffa53e69014917d5
piontec <piontec@gmail.com>
2013-04-08 22:53:47 +0200

    CHG: inteligo test stub

--- Bank2QifTests/src/ConvertersTests/InteligoCsvParsersTest.cs
+++ Bank2QifTests/src/ConvertersTests/InteligoCsvParsersTest.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Bank2Qif.Converters;
+using Bank2Qif.Converters.Inteligo;
 using NUnit.Framework;
 
 namespace Bank2QifTests.ConvertersTests
-{
+{   
+    [TestFixture]
     public class InteligoCsvParsersTest
     {
-        
+        private const string testLines =
+        "\"Id\",\"Data księgowania\",\"Data zlecona\",\"Typ transakcji\",\"Kwota\",\"Waluta\",\"Saldo po transakcji\",\"Rachunek nadawcy/odbiorcy\",\"Nazwa nadawcy/odbiorcy\",\"Opis transakcji\""
+        + "\n\r"
+        + "\"1111\",\"2012-01-11\",\"2012-01-11\",\"Przelew z rachunku\",\"-100.00\",\"PLN\",\"200.00\",\"11222233334444555566667777\",\"XXXX YYYYY  ZZZZZ, WWWWWW ZIP City\",\"XXXX YYYYY  ZZZZZ, WWWWWW ZIP City\",\"XX XXXXXX: 11112222\",\"XX XXXX.: 11222233334444555566667777\",\"XXXXXX: XX XXXX XX XXXXX\",\"XXXXX XXXXXX: 2012-01-11\",\"XXXXXX XXXXXXXXXXXX\"";
+    
+        private IConverter m_converter;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_converter = new InteligoCsvToQif();
+        }
+
+
+        [Test]
+        public void ShouldParseOneEntry()
+        {
+            var entries = m_converter.ConvertLinesToQif(testLines);
+            Assert.AreEqual (entries.Count (), 1);
+        }
     }
 }
