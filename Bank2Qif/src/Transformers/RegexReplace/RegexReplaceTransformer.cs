c3fb94d876b8c1dd43626146b5d6fd3755ff52db
piontec <piontec@gmail.com>
2013-04-15 23:01:41 +0200

    ADD: RegexReplaceTransformer

---
+++ Bank2Qif/src/Transformers/RegexReplace/RegexReplaceTransformer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Nini.Config;
+
+namespace Bank2Qif.Transformers.RegexReplace
+{
+    [Transformer (110)]
+    public class DefaultTransformer : BaseTransformer, ITransformer
+    {
+        public DefaultTransformer(IConfig config)
+        {                 
+            Initialize(config);
+        }
+
+        public IEnumerable<QifEntry> Transform(IEnumerable<QifEntry> entries)
+        {
+            foreach (var entry in entries)
+            {
+                entry.Description = ReplaceMultispace(entry.Description);
+                entry.Payee = ReplaceMultispace(entry.Payee);
+            }
+
+            return entries;
+        }
+
+        private string ReplaceMultispace(string text)
+        { 
+            return Regex.Replace(text, " +", " ");
+        }    
+    }
+}
